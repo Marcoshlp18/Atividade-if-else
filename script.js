84957a3ba8352b53def90349642388c5d72120dc
Marcoshlp18 <marcoshenriqueball12@gmail.com>
2022-06-11 08:59:53 -0300

    Atividade de condição

---
+++ script.js
@@ -0,0 +1,37 @@
+let idade="18";
+if (idade >="18") {
+  console.log("Pode tirar CNH");
+}
+let maiorDeIdade = true;
+
+if(maiorDeIdade){
+  console.log("Sou maior de idade");
+}else{
+  console.log("Ainda não sou maior de idade");
+}
+let mêsDeAniversário="Maio";
+
+if( (mêsDeAniversário =="Janeiro") ||(mêsDeAniversário =="Dezembro") ){
+  console.log("Parabens!");
+}else{
+  console.log("Mentiroso mija na cama!");
+}
+let SeuNome="Rian";
+
+if (SeuNome.indexOf("R")) {
+  console.log("Sabia que a letra R é a letra 18 do alfabeto ?");
+}
+
+
+
+let str= "Hello World!";
+        if(str.contains("World")) 
+        {
+          console.log("It is true");
+        }
+        else
+        {
+          console.log("It is false");  
+        }
+
+
